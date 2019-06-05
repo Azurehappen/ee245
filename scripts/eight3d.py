878af4518d8fd774e2e9d253fb3f57df7aa4187d
Azurehappen <45580484+Azurehappen@users.noreply.github.com>
2019-06-05 16:42:06 -0700

    Add files via upload

--- scripts/eight3d.py
+++ scripts/eight3d.py
@@ -16,7 +16,12 @@ if __name__ == '__main__':
     cf.setParam("stabilizer/estimator", 2) # Use EKF
     cf.setParam("stabilizer/controller", 2) # Use mellinger controller
     #cf.setParam("ring/effect", 7)
-
+    
+    alp,beta,phi = np.pi*20/180,np.pi*10/180,np.pi*10/180
+    Rx = np.array([[1,0,0],[0,np.cos(alp),-np.sin(alp)],[0,np.sin(alp),np.cos(alp)]])
+    Ry = np.array([[np.cos(beta),0,np.sin(beta)],[0,1,0],[-np.sin(beta),0,np.cos(beta)]])
+    Rz = np.array([[np.cos(phi),-np.sin(phi),0],[np.sin(phi),np.cos(phi),0],[0,0,1]])
+    R_bs = Rx.dot(Ry).dot(Rz).T
     cf.takeoff(targetHeight = 1.0, duration = 6.0)
     time.sleep(6.0)
 
@@ -25,10 +30,11 @@ if __name__ == '__main__':
     r,num=0.5,72
     theta = np.array(range(num-1)).astype(float)*2*np.pi/num
     org = np.array([0,1.5,1])
-    R_bs = np.array([[0.933,0.3491,-0.0871],[0.25,0.87,0.425],[0.2588,-0.483,0.8365]])
+    #R_bs = np.array([[0.933,0.3491,-0.0871],[-0.25,0.803,0.541],[0.2588,-0.483,0.8365]])
     for th in theta:
         augm = np.array([r-r*np.cos(th),0,r*np.sin(th)]).reshape(3,1)
         goal = org + R_bs.dot(augm).reshape(3)
+        print(goal)
         cf.cmdPosition(list(np.round(goal,3)),yaw=0)
         time.sleep(0.1)
     cf.cmdPosition(org,yaw=0)
@@ -36,6 +42,7 @@ if __name__ == '__main__':
     for th in theta:
         augm = np.array([r*np.cos(th)-r,0,r*np.sin(th)]).reshape(3,1)
         goal = org + R_bs.dot(augm).reshape(3)
+        print(goal)
         cf.cmdPosition(list(np.round(goal,3)),yaw=0)
         time.sleep(0.1)
     cf.cmdPosition(org,yaw=0)
